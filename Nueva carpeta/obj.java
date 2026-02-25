6a5d59b6f53be34c2ff25137da6539d9665c79a4
dennisgarro <sistemasitm6099@gmail.com>
2026-02-24 20:55:25 -0500

    Add files via upload

---
+++ Nueva carpeta/obj.java
@@ -0,0 +1,15 @@
+public class obj {
+    private int dato;
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public obj() {
+    }
+    
+}
